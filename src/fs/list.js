ec30d45911b6f0399c834f078c35807cb6a0b138
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-02 22:01:19 +0200

    feat: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { destinationFolder, errorMessage } from '../utils/workWithFiles.js';
+
 const list = async () => {
-    // Write your code here 
+    const currDir = dirname(process.argv[1]);
+    const src = join(currDir, destinationFolder);
+    try {
+        const files = await readdir(src);
+        console.log(files);
+
+    } catch {
+        throw new Error(errorMessage);
+    }
 };
 
 await list();
