94b8de75637a1b5babadaf34d45ccc947ddc5094
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-02 14:10:49 +0200

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { URL } from 'url';
+
+const file = {
+    name: 'fresh.txt',
+    parentFolder: 'files',
+    content: 'I am fresh and young',
+    errorMessage: 'FS operation failed'
+};
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(new URL(import.meta.url).pathname);
+    const folderPath = path.join(__dirname, file.parentFolder);
+    const filePath = path.join(folderPath, file.name);
+
+    try {
+        await fs.writeFile(filePath, file.content, { flag: 'wx' });
+    } catch {
+        throw new Error(file.errorMessage);
+    }
 };
 
 await create();
