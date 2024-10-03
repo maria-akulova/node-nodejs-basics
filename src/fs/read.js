3196c83b3c2522062d4e402bf08c141550289549
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-03 10:22:50 +0200

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { destinationFolder, errorMessage } from '../utils/workWithFiles.js';
+
 const read = async () => {
-    // Write your code here 
+    const currDir = dirname(process.argv[1]);
+    const src = join(currDir, destinationFolder, 'fileToRead.txt');
+
+    try {
+        const content = await readFile(src, { encoding: 'utf8' });
+        console.log(content);
+    } catch {
+        throw new Error(errorMessage);
+    }
 };
 
 await read();
