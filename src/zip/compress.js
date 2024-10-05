6235b6bb4e608a7fe59eb18248a7fdf2fc6c0f8a
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-05 14:40:51 +0200

    feat: zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createWriteStream, createReadStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { destinationFile } from '../utils/workWithFiles.js';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { destinationFolder } from '../utils/workWithFiles.js';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const fileToZip = destinationFile('fileToCompress.txt');
+    const currDir = dirname(process.argv[1]);
+    const zippedFile = join(currDir, destinationFolder, 'archive.gz');
+
+    const src = createReadStream(fileToZip);
+    const destination = createWriteStream(zippedFile);
+
+    try {
+        pipeline(src, gzip, destination);
+    } catch (error) {
+        throw new Error('Pipeline failed.', err);
+    }
 };
 
 await compress();
