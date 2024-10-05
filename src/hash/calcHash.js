f871bcbf5e6b0e058fe329e64fc982104ec4d4bf
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-05 21:59:09 +0200

    fix: hash output

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,18 +1,27 @@
 import { createReadStream } from 'node:fs';
 import { createHash } from 'node:crypto';
 import { stdout } from 'node:process';
-import { finished } from 'node:stream/promises';
-
+import { pipeline } from 'node:stream/promises';
 import { destinationFile } from '../utils/workWithFiles.js';
 
 const calculateHash = async () => {
     const src = destinationFile('fileToCalculateHashFor.txt');
     const hash = createHash('sha256');
     const input = createReadStream(src);
-    input.pipe(hash).setEncoding('hex').pipe(stdout);
 
-    await finished(input).catch(console.error);
-    input.resume();
+    try {
+        await pipeline(
+            input,
+            hash,
+            async function (src) {
+                src.setEncoding('hex');
+                return src.pipe(stdout);
+            }
+        );
+        console.log('\n');
+    } catch (err) {
+        console.error('Error during hash calculation:', err);
+    }
 };
 
 await calculateHash();
