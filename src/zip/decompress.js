fde7cc5558cd1cf940417288de4098b58a86b0f7
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-05 15:10:20 +0200

    feat: zip/decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createWriteStream, createReadStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { destinationFile } from '../utils/workWithFiles.js';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { destinationFolder } from '../utils/workWithFiles.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+
+    const zippedFile = destinationFile('archive.gz');
+    const currDir = dirname(process.argv[1]);
+    const unzippedFile = join(currDir, destinationFolder, 'fileToCompress.txt');
+
+    const src = createReadStream(zippedFile);
+    const destination = createWriteStream(unzippedFile);
+
+    try {
+        pipeline(src, unzip, destination)
+
+    } catch (err) {
+        throw new Error('Pipeline failed.', err);
+    }
 };
 
 await decompress();
